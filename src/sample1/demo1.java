68b0378acbe524f767008973b04b6db9f30e973a
admin <admin@DESKTOP-S64H8J8>
2022-12-06 13:12:49 +0530

    [lobhesh]:add new project

---
+++ src/sample1/demo1.java
@@ -0,0 +1,11 @@
+package sample1;
+
+public class demo1 
+{
+   public static void main(String[] args) {
+	
+	   System.out.println("hello world");
+	   
+	   System.out.println("hellow world");
+}
+}
